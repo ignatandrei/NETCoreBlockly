2cada8892982eb8216c4dcb0d71c5f53b3930139
ignatandrei <ignatandrei@yahoo.com>
2020-04-18 14:25:58 +0300

    creating zip

--- src/NetCore2Blockly/createHTML.ps1
+++ src/NetCore2Blockly/createHTML.ps1
@@ -1,26 +1,19 @@
+$folder= ".\TestBlocklyHtml\wwwroot"
+$fileContent = Get-Content "$folder\blockly.html"
 
-function GetStringBetweenTwoStrings($firstString, $secondString, $importPath){
+$fileContent = $fileContent.Replace('<!--doNotCopyToFinal-->','<!--')
+$fileContent = $fileContent.Replace('<!--enddoNotCopyToFinal-->','-->')
+$fileContent = $fileContent.Replace('/*doNotCopyToFinal*/','/*')
+$fileContent = $fileContent.Replace('/*enddoNotCopyToFinal*/','*/')
 
-    #Get content from file
-    $file = Get-Content $importPath
-	Write-Host $file 
-	#$file = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-    #Regex pattern to compare two strings
-[regex]$regex = 
-@'
-(?ms)doNotCopy:
-(.+?)
-endDoNotCopy:.+?   
-'@;
-$regex.Matches("$(Get-Content $importPath -Raw)") |
- foreach { $_.groups[1].value }{
-	Write-Host $_
- }
-    #Return result
-    return 10
-
+Out-File -InputObject $fileContent  -FilePath ".\TestBlocklyHtml\wwwroot\blockly.html"
+$zipFile = "$folder\blocklyNetCore.zip"
+$compress = @{
+  Path = "$Folder"
+  CompressionLevel = "Fastest"
+  DestinationPath = $zipFile
 }
-$myText =GetStringBetweenTwoStrings -firstString 'doNotCopy' -secondString 'endDoNotCopy' -importPath  ".\TestBlocklyHtml\wwwroot\blockly.html"
-$myText =GetStringBetweenTwoStrings -firstString 'Lorem' -secondString 'is' -importPath  ".\TestBlocklyHtml\wwwroot\blockly.html"
+Compress-Archive @compress
+
+ 
 Write-Host "-====="
-Write-Host $myText
