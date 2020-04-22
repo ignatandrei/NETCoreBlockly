df965aaf9975f1a3fe424db8c97977aa77431c43
Cosmin Popescu <cosmin.popescu93@gmail.com>
2020-04-22 22:54:25 +0300

    Separated XHR POST

--- src/NetCore2Blockly/TestBlocklyHtml/wwwroot/blockly/BlocklyXHRWrapper.js
+++ src/NetCore2Blockly/TestBlocklyHtml/wwwroot/blockly/BlocklyXHRWrapper.js
@@ -1,7 +1,6 @@
 ﻿// XHR wrapper functions
 
 const doGet = (href, callback) => {
-
     console.log(href, callback);
     let req = new XMLHttpRequest();
     req.open('GET', href, true);
@@ -12,11 +11,38 @@ const doGet = (href, callback) => {
             } else {
                 throw `${href} status :${req.status}`;
             }
-
         }
         else {
             //window.alert(`error ${href} ${req.status}`);
         }
     };
     req.send(null);
+}
+
+const doPost = (href, objectToPost, callback) => {
+    let data = objectToPost;
+    console.log(`sending ${data}`);
+    let req = new XMLHttpRequest();
+    req.open('POST', href, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status >= 200 && req.status < 300) {
+                var answer = req.responseText;
+                if (answer) {
+                    return callback(answer);
+                }
+                else {
+                    return callback(true);
+                }
+            } else {
+                throw `${href} status :${req.status}`;
+            }
+        }
+        else {
+            //window.alert(`error ${href} ${req.status}`);
+        }
+    };
+    req.send(data);
 }
