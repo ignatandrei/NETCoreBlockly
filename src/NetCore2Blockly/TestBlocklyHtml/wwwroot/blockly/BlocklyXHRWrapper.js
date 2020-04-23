bbc3bff1bf959f73fe9e598b9ea2beb276a241c0
Cosmin Popescu <cosmin.popescu93@gmail.com>
2020-04-23 09:19:18 +0300

    Extracted put and delete into XHR Wrapper

--- src/NetCore2Blockly/TestBlocklyHtml/wwwroot/blockly/BlocklyXHRWrapper.js
+++ src/NetCore2Blockly/TestBlocklyHtml/wwwroot/blockly/BlocklyXHRWrapper.js
@@ -46,3 +46,60 @@ const doPost = (href, objectToPost, callback) => {
     };
     req.send(data);
 }
+
+const doPut = (href, objectToPost, callback) => {
+    let data = objectToPost;
+    //console.log(`sending ${data}`);
+    let req = new XMLHttpRequest();
+
+    req.open('PUT', href, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status >= 200 && req.status < 300) {
+                var answer = req.responseText;
+                if (answer) {
+                    return callback(answer);
+                }
+                else {
+                   return callback(true);
+                }
+            } else {
+                throw `${href} status :${req.status}`;
+            }
+
+        }
+        else {
+            //window.alert(`error ${href} ${req.status}`);
+        }
+    };
+    req.send(data);
+}
+
+const doDelete = (href, callback) => {
+    var req = new XMLHttpRequest();
+
+    req.open('DELETE', href, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status >= 200 && req.status < 300) {
+                var answer = req.responseText;
+                if (answer) {
+                    callback(answer);
+                }
+                else {
+                    callback(true);
+                }
+            } else {
+                throw `${href} status :${req.status}`;
+            }
+        }
+        else {
+            //window.alert(`error ${href} ${req.status}`);
+        }
+    };
+    req.send(null);
+}
