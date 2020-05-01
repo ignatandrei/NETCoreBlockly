e6a861183c4cd7e9b774f887e6b187db8ddd459a
ignatandrei <ignatandrei@yahoo.com>
2020-05-01 15:10:20 +0300

    treid nullable

---
+++ src/NetCore2Blockly/TestBlocklyHtml/Controllers/TestNullableController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestBlocklyHtml.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestNullableController : ControllerBase
+    {
+
+        [HttpGet("{id?}")]
+        public string ActionWithNullParameter(int? id)
+        {
+            return (id == null) ? "no parameter" : $"parameter {id}";
+        }
+    }
+}
