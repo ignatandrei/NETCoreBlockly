753112211a51082c8b26d0b253312ebbf54e36a1
ignatandrei <ignatandrei@yahoo.com>
2020-05-15 08:32:57 +0300

    test with various parameters

--- src/NetCore2Blockly/TestBlocklyHtml/Controllers/TestNullableController.cs
+++ src/NetCore2Blockly/TestBlocklyHtml/Controllers/TestNullableController.cs
@@ -9,13 +9,19 @@ namespace TestBlocklyHtml.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class TestNullableController : ControllerBase
+    public class VariousTestsController : ControllerBase
     {
 
         [HttpGet("{id?}")]
         public string ActionWithNullParameter(int? id)
         {
             return (id == null) ? "no parameter" : $"parameter {id}";
+        }
+
+        [HttpGet("{id}")]
+        public string ActionWith2ParametersAndARoute(int id, int x, int y)
+        {
+            return $"received route {id} and parameters {x} {y}";
         }
     }
 }
