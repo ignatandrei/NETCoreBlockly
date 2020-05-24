928def4828a5bc857d38fde38e1b1984dd5e3c35
ignatandrei <ignatandrei@yahoo.com>
2020-05-24 20:40:36 +0300

    add services

--- src/NetCore2Blockly/TestBlocklyHtml/Startup.cs
+++ src/NetCore2Blockly/TestBlocklyHtml/Startup.cs
@@ -41,6 +41,7 @@ namespace TestBlocklyHtml
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddProblemDetails();
             services.AddCors(options =>
             {
                 options.AddPolicy(name: "AllowAll",
