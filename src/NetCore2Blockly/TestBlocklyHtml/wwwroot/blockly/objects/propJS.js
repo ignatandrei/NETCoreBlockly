d33567ffe53567352496055dc521102101ec79bd
ignatandrei <ignatandrei@yahoo.com>
2020-05-24 18:28:00 +0300

    refactor

---
+++ src/NetCore2Blockly/TestBlocklyHtml/wwwroot/blockly/objects/propJS.js
@@ -0,0 +1,59 @@
+Blockly.Blocks['modifyproperty'] = {
+            init: function () {
+                this.appendDummyInput()
+                    .appendField("Modify ");
+                this.appendValueInput("ObjectToChange")
+                    .setCheck(null)
+                    .setAlign(Blockly.ALIGN_CENTRE)
+                    .appendField(new Blockly.FieldLabelSerializable("object"), "objectName");
+                this.appendValueInput("PropertyName")
+                    .setCheck(null)
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(new Blockly.FieldLabelSerializable(",property"), "prop");
+                this.appendValueInput("NewValue")
+                    .setCheck(null)
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(new Blockly.FieldLabelSerializable("toValue"), "newValue");
+                this.setInputsInline(true);
+                this.setPreviousStatement(true, null);
+                this.setNextStatement(true, null);
+                this.setTooltip("");
+                this.setHelpUrl("");
+            }
+        };
+        Blockly.JavaScript['modifyproperty'] = function (block) {
+            var value_objecttochange = Blockly.JavaScript.valueToCode(block, 'ObjectToChange', Blockly.JavaScript.ORDER_ATOMIC);
+            var value_propertyname = Blockly.JavaScript.valueToCode(block, 'PropertyName', Blockly.JavaScript.ORDER_ATOMIC);
+            var value_newvalue = Blockly.JavaScript.valueToCode(block, 'NewValue', Blockly.JavaScript.ORDER_ATOMIC);
+            var code = value_objecttochange + "[" + value_propertyname + ']=' + value_newvalue + ";";
+            return code;
+        };
+        Blockly.Blocks['getproperty'] = {
+            init: function () {
+                this.appendDummyInput()
+                    .appendField("Get from");
+                this.appendValueInput("ObjectToChange")
+                    .setCheck(null)
+                    .setAlign(Blockly.ALIGN_CENTRE)
+                    .appendField(new Blockly.FieldLabelSerializable("object"), "objectName");
+                this.appendValueInput("PropertyName")
+                    .setCheck(null)
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(new Blockly.FieldLabelSerializable("property"), "prop");
+                this.setInputsInline(true);
+                this.setOutput(true, null)
+                //this.setTooltip("");
+                //this.setHelpUrl("");
+            }
+
+        };
+        Blockly.JavaScript['getproperty'] = function (block) {
+            var value_objecttochange = Blockly.JavaScript.valueToCode(block, 'ObjectToChange', Blockly.JavaScript.ORDER_ATOMIC);
+            var value_propertyname = Blockly.JavaScript.valueToCode(block, 'PropertyName', Blockly.JavaScript.ORDER_ATOMIC);
+
+            var code = value_objecttochange + "[" + value_propertyname + ']';
+            // TODO: Change ORDER_NONE to the correct strength.
+            return [code, Blockly.JavaScript.ORDER_NONE];
+        };
+
+		
