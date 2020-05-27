0a6d3e96e57c259730af5b8f79b93ffd4fb6cd26
Cosmin Popescu <cosmin.popescu93@gmail.com>
2020-05-27 08:38:31 +0300

    Basic EF configuration added

---
+++ src/NetCore2Blockly/GraphQLDemo/Service/GraphQLFillDb.cs
@@ -0,0 +1,27 @@
+﻿using GraphQLDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLDemo.Service
+{
+    public partial class GraphQLDbContext : DbContext
+    {
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().
+                HasData(new Department { Iddepartment = 1, Name = "IT" },
+                new Department { Iddepartment = 2, Name = "Accounting" }
+                );
+
+            modelBuilder.Entity<Employee>().
+                HasData(new Employee { Iddepartment = 1, Name = "Person from IT", Idemployee = 1 },
+                new Employee { Iddepartment = 2, Name = "Person from Accounting", Idemployee = 2 }
+                );
+
+
+        }
+    }
+}
