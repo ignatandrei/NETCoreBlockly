2dfea35d463785dc78a665867a56af012dc5967b
ignatandrei <ignatandrei@yahoo.com>
2020-06-04 08:07:41 +0300

    more logging

--- src/NetCore2Blockly/copyToRoot.ps1
+++ src/NetCore2Blockly/copyToRoot.ps1
@@ -1,3 +1,6 @@
 Move-Item .\licenses.json ..\..\licencesNetCore.json
+Write-Host 'copied licenses.json '
 Move-Item .\outdated.csv ..\..\outdatedNetCore.csv
+Write-Host 'copied outdated.csv '
 Move-Item .\thanks.txt ..\..\thanksNetCore.txt
+Write-Host 'copied thanks.txt '
