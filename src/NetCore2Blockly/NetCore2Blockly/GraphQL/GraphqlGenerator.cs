d248aa34af441ba5411910578245e9a56838e2c3
Cosmin Popescu <cosmin.popescu93@gmail.com>
2020-06-14 16:37:40 +0300

    Created ActionInfo

--- src/NetCore2Blockly/NetCore2Blockly/GraphQL/GraphqlGenerator.cs
+++ src/NetCore2Blockly/NetCore2Blockly/GraphQL/GraphqlGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 
@@ -17,6 +18,8 @@ namespace NetCore2Blockly.GraphQL
         }
         public async Task<GraphQLActionInfo[]> GetIntrospection()
         {
+            List<GraphQLActionInfo> arrayOfActions = new List<GraphQLActionInfo>();
+
             var schema = "{\r\n  __schema {\r\n    queryType {\r\n      name\r\n    }\r\n    mutationType {\r\n      name\r\n    }\r\n    subscriptionType {\r\n      name\r\n    }\r\n    types {\r\n      ...FullType\r\n    }\r\n    directives {\r\n      name\r\n      description\r\n      locations\r\n      args {\r\n        ...InputValue\r\n      }\r\n    }\r\n  }\r\n}\r\n\r\nfragment FullType on __Type {\r\n  kind\r\n  name\r\n  description\r\n  fields(includeDeprecated: true) {\r\n    name\r\n    description\r\n    args {\r\n      ...InputValue\r\n    }\r\n    type {\r\n      ...TypeRef\r\n    }\r\n    isDeprecated\r\n    deprecationReason\r\n  }\r\n  inputFields {\r\n    ...InputValue\r\n  }\r\n  interfaces {\r\n    ...TypeRef\r\n  }\r\n  enumValues(includeDeprecated: true) {\r\n    name\r\n    description\r\n    isDeprecated\r\n    deprecationReason\r\n  }\r\n  possibleTypes {\r\n    ...TypeRef\r\n  }\r\n}\r\n\r\nfragment InputValue on __InputValue {\r\n  name\r\n  description\r\n  type {\r\n    ...TypeRef\r\n  }\r\n  defaultValue\r\n}\r\n\r\nfragment TypeRef on __Type {\r\n  kind\r\n  name\r\n  ofType {\r\n    kind\r\n    name\r\n    ofType {\r\n      kind\r\n      name\r\n      ofType {\r\n        kind\r\n        name\r\n        ofType {\r\n          kind\r\n          name\r\n          ofType {\r\n            kind\r\n            name\r\n            ofType {\r\n              kind\r\n              name\r\n              ofType {\r\n                kind\r\n                name\r\n              }\r\n            }\r\n          }\r\n        }\r\n      }\r\n    }\r\n  }\r\n}";
 
             string endpointWithQuery = Endpoint + "?query=";
@@ -43,17 +46,24 @@ namespace NetCore2Blockly.GraphQL
 
                 Console.WriteLine(schemaObjectsToString);
                 var obj = Root.FromJson(schemaObjectsToString);//works
-                // Console.WriteLine(obj.Name);
                 string controllerName = obj.Name;
                 foreach(var f in obj.Fields)
                 {
                     // generate GraphQLActionInfo
+                    var action = new GraphQLActionInfo();
+                    action.ControllerName = controllerName;
+                    action.ActionName = f.Name;
 
+                    arrayOfActions.Add(action);
                     // from the field args generate grpahtqltypearguments
+
+                    action.RelativeRequestUrl = f.Name;
+                    action.Verb  =  "GET";
+                    
                 }
             }
             //return the array of ACtion Info
-            return null;
+            return arrayOfActions.ToArray();
         }
     }
 }
