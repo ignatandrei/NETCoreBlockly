a4a039dfd3c7638289fd5f6f84e26f130e239a5b
ignatandrei <ignatandrei@yahoo.com>
2020-06-23 20:53:17 +0300

    with credentials

--- src/NetCore2Blockly/TestBlocklyHtml/wwwroot/blockly/Http/BlocklyXHRWrapper.js
+++ src/NetCore2Blockly/TestBlocklyHtml/wwwroot/blockly/Http/BlocklyXHRWrapper.js
@@ -3,9 +3,10 @@
 const doGet = (href, callback, headers, withCreds) => {
     console.log(href, callback);
     let req = new XMLHttpRequest();
-    req.withCredentials = withCreds;
+    
     req.open('GET', href, true);
-	if(headers.length>0){
+    req.withCredentials = withCreds;
+    if (headers.length > 0) {
 		//alert(JSON.stringify(headers));
 		for(var iHeader=0;iHeader<headers.length;iHeader++){
 			var head=headers[iHeader];
@@ -47,12 +48,20 @@ const doGet = (href, callback, headers, withCreds) => {
     req.send(null);
 }
 
-const doPost = (href, objectToPost, callback) => {
+const doPost = (href, objectToPost, callback, headers, withCreds) => {
     let data = objectToPost;
     console.log(`sending ${data}`);
     let req = new XMLHttpRequest();
     req.open('POST', href, true);
     req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.withCredentials = withCreds;
+    if (headers.length > 0) {
+        //alert(JSON.stringify(headers));
+        for (var iHeader = 0; iHeader < headers.length; iHeader++) {
+            var head = headers[iHeader];
+            req.setRequestHeader(head.name, head.value);
+        }
+    }
 
     req.onreadystatechange = function () {
         if (req.readyState == 4) {
@@ -88,13 +97,21 @@ const doPost = (href, objectToPost, callback) => {
     req.send(data);
 }
 
-const doPut = (href, objectToPost, callback) => {
+const doPut = (href, objectToPost, callback, headers, withCreds) => {
     let data = objectToPost;
     //console.log(`sending ${data}`);
     let req = new XMLHttpRequest();
 
     req.open('PUT', href, true);
     req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.withCredentials = withCreds;
+    if (headers.length > 0) {
+        //alert(JSON.stringify(headers));
+        for (var iHeader = 0; iHeader < headers.length; iHeader++) {
+            var head = headers[iHeader];
+            req.setRequestHeader(head.name, head.value);
+        }
+    }
 
     req.onreadystatechange = function () {
         if (req.readyState == 4) {
@@ -129,13 +146,21 @@ const doPut = (href, objectToPost, callback) => {
     };
     req.send(data);
 }
-const doPatch = (href, objectToPost, callback) => {
+const doPatch = (href, objectToPost, callback, headers, withCreds) => {
     let data = objectToPost;
     //console.log(`sending ${data}`);
     let req = new XMLHttpRequest();
 
     req.open('PATCH', href, true);
     req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.withCredentials = withCreds;
+    if (headers.length > 0) {
+        //alert(JSON.stringify(headers));
+        for (var iHeader = 0; iHeader < headers.length; iHeader++) {
+            var head = headers[iHeader];
+            req.setRequestHeader(head.name, head.value);
+        }
+    }
 
     req.onreadystatechange = function () {
         if (req.readyState == 4) {
@@ -171,11 +196,19 @@ const doPatch = (href, objectToPost, callback) => {
     req.send(data);
 }
 
-const doDelete = (href, callback) => {
+const doDelete = (href, callback, headers, withCreds) => {
     var req = new XMLHttpRequest();
 
     req.open('DELETE', href, true);
     //req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.withCredentials = withCreds;
+    if (headers.length > 0) {
+        //alert(JSON.stringify(headers));
+        for (var iHeader = 0; iHeader < headers.length; iHeader++) {
+            var head = headers[iHeader];
+            req.setRequestHeader(head.name, head.value);
+        }
+    }
 
     req.onreadystatechange = function () {
         if (req.readyState == 4) {
