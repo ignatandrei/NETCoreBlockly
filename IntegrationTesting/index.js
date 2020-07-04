b02b6a95e31569e82eb0891e493f0d32020f1a71
ignatandrei <ignatandrei@yahoo.com>
2020-07-04 07:26:40 +0300

    adding tests

--- IntegrationTesting/index.js
+++ IntegrationTesting/index.js
@@ -1,21 +1,47 @@
 const playwright = require('playwright');
 console.log('start');
 (async () => {
-    for (const browserType of ['chromium', 'firefox'/*, 'webkit'*/]) {
+    for (const browserType of ['chromium'/*, 'firefox', 'webkit'*/]) {
       const browser = await playwright[browserType].launch();
       const context = await browser.newContext();
-      const page = await context.newPage();
+      var page = await context.newPage();
+      await page.goto('https://netcoreblockly.herokuapp.com/');
+      page = await context.newPage();
       await page.goto('https://netcoreblockly.herokuapp.com/blockly.html');
-      await page.keyboard.press('Escape');
-      await clickExecute('firstDemo',page,browserType);
 
+      await page.keyboard.press('Escape');
+      //await clickExecute('firstDemo',page,browserType);
+      //const text = await page.evaluate(() => Array.from(document.querySelectorAll('#results > li'), element => element));//.textContent
+      const allTests = await page.$$('#results > li');
+      console.log(allTests.length);
+      for(var i=0;i<allTests.length;i++){
+        var nr= (i+1).toString().padStart(3,'0');
+        var li  = allTests[i];
+        await li.scrollIntoViewIfNeeded();
+        var text=await li.$("a");
+        text=await text.getProperty("innerHTML");
+        text = await text.jsonValue();
+        text = "Test_" + nr +"_"+text;
+        //console.log(text);
+        await li.click();
+        await sleep(1000);
+        await clickExecute(text,page,browserType);
+        break;
+      }
       
+      
+            //await text[3].click();
+      //.await clickExecute('firstDemo',page,browserType);
+      // const handle = await page.$('//ul[@id="results"]/li');
+      // console.log(handle);
+      // var prop=await handle.getProperty("value");
+      // console.log(await prop.jsonValue());
       await browser.close();
     }
   })();
 
   async function clickExecute(name, page, browserType){
-
+    console.log(`start ${name} ${browserType}`);
     await page.click('text=E X E C U T E');
 
     await sleep(5000);
