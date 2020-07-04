184882cb464741508044ad518729ea163ae4cea8
ignatandrei <ignatandrei@yahoo.com>
2020-07-04 09:10:14 +0300

    display text in grid

--- src/NetCore2Blockly/TestBlocklyHtml/wwwroot/extensions/resultsGrid.js
+++ src/NetCore2Blockly/TestBlocklyHtml/wwwroot/extensions/resultsGrid.js
@@ -1,6 +1,7 @@
 
 var dataObject = [];
 var objectsForGrid = [];
+var stringsForGrid = [];
 var hot;
 function initGrid(gridElement) {
     //console.log(gridElement);
@@ -34,10 +35,12 @@ function initGrid(gridElement) {
     hot.redraw(true);
 }
 function AddStringToGrid(value) {
+    //window.alert(value);
     if (value.startsWith("{") && value.endsWith("}")) {
         try {
             var obj = JSON.parse(value);
             objectsForGrid.push(obj);
+            return;
         }
         catch(err)  {
             //do nothing
@@ -49,13 +52,15 @@ function AddStringToGrid(value) {
             for (var i = 0; i < arr.length; i++) {
                 objectsForGrid.push(arr[i]);
             };
+            return;
         }
         catch(err) {
             //do nothing
         }
 
     };
-
+    stringsForGrid.push(value);
+    //window.alert('length:'+stringsForGrid.length);
 }
 function AddDataToGrid(value, gridElement) {
     dataObject.push([value]);
@@ -71,10 +76,12 @@ function AddDataToGrid(value, gridElement) {
         AddStringToGrid(JSON.stringify(value));
         return;
     }
+    AddStringToGrid(value.toString());
 }
 function ClearDataGrid() {
     dataObject = [];
     objectsForGrid = [];
+    stringsForGrid = [];
     if (hot != null) {
         
         hot.setColumns([{ title: 'Step', field: 'id' }, { title: 'Value', field: 'val' }]);
@@ -87,15 +94,11 @@ function ClearDataGrid() {
     }
 
 }
-function FinishGrid() {
-
-    if (objectsForGrid.length == 0)
-        return;
-
+function AddObjectToFinishGrid() {
     var headers = [];
     var obj = objectsForGrid;
     //console.log("object !", obj);
-    
+
     for (var i = 0; i < obj.length; i++) {
         var data = obj[i];
         if (typeof data === 'string') {
@@ -108,13 +111,13 @@ function FinishGrid() {
     //console.log("headers 1", headers);
     var mySet = new Set(headers);
     headers = Array.from(mySet);
-    
+
     var allHeaders = [...headers];
     var fullData = [];
     for (var i = 0; i < obj.length; i++) {
         var data = obj[i];
         //console.log(data);
-        var res = { Nr : i + 1 };
+        var res = { Nr: i + 1 };
         //res["_children"] = [];
         for (var p = 0; p < headers.length; p++) {
             var key = headers[p];
@@ -124,19 +127,19 @@ function FinishGrid() {
                 var val = data[key];
                 if (typeof val === "object") {
                     res[defKey] = JSON.stringify(val);
-                    
+
                     //res["_children"].push(val);
                     //allHeaders.push(...Object.keys(val));
                 }
                 else
-                    res[defKey]=val;
+                    res[defKey] = val;
             }
             else {
-                if (typeof data === 'string' && p===0) {
-                    res[defKey]=data;
+                if (typeof data === 'string' && p === 0) {
+                    res[defKey] = data;
                 }
                 else {
-                    res[defKey]='';
+                    res[defKey] = '';
                 }
             }
         }
@@ -172,10 +175,10 @@ function FinishGrid() {
                             var id = col + "_" + row;
                             onRendered(function () {
                                 //window.alert('test');
-                                var table = new Tabulator("#"+id, {
+                                var table = new Tabulator("#" + id, {
                                     data: arr,
                                     autoColumns: true,
-                                    layout: "fitDataFill",   
+                                    layout: "fitDataFill",
                                     headerSort: false,
                                     tooltips: function (cell) {
                                         return cell.getColumn().getField() + " - " + JSON.stringify(cell.getValue()); //return cells "field - value";
@@ -203,6 +206,86 @@ function FinishGrid() {
     //window.alert(JSON.stringify(fullData));
     hot.setColumns(hs);
     hot.replaceData(fullData);
+}
+function AddStringToFinishGrid() {
+
+    var fullData = [];
+    for (var i = 0; i < stringsForGrid.length; i++) {
+        fullData.push({ Nr: i + 1, Text: stringsForGrid[i] });
+    }
+    //window.alert(fullData.length);
+    var allHeaders = ["Nr", "Text"];
+    var hs = allHeaders.map(it => {
+        return {
+
+            cellClick: function (e, cell) {
+                var row = cell.getRow().getData().Nr;
+                var col = cell.getColumn().getField();
+                alert(`The row at : ${row} , col: ${row} has value :\n ` + cell.getValue()); //display the cells value
+            },
+            title: it,
+            field: goodNameForKey(it),
+            headerFilter: true,
+            formatter: function (cell, formatterParams, onRendered) {
+                //cell - the cell component
+                //formatterParams - parameters set for the column
+                //onRendered - function to call when the formatter has been rendered
+                try {
+                    var value = cell.getValue().toString();
+                    //return value;
+                    if (value.length < 2)
+                        return value;
+                    if (value.startsWith("[") && value.endsWith("]")) {
+                        try {
+                            var arr = JSON.parse(value);
+                            var row = cell.getRow().getData().Nr;
+                            var col = cell.getColumn().getField();
+                            var id = col + "_" + row;
+                            onRendered(function () {
+                                //window.alert('test');
+                                var table = new Tabulator("#" + id, {
+                                    data: arr,
+                                    autoColumns: true,
+                                    layout: "fitDataFill",
+                                    headerSort: false,
+                                    tooltips: function (cell) {
+                                        return cell.getColumn().getField() + " - " + JSON.stringify(cell.getValue()); //return cells "field - value";
+                                    }
+                                });
+                            });
+                            return "<div id='" + id + "'>" + value + "</div>";
+
+                        }
+                        catch (err) {
+                            return value;
+                        }
+
+                    };
+                }
+                catch (e) {
+                    return value;
+                }
+                return cell.getValue();
+            }
+        }
+    });
+    hot.setColumns(hs);
+    hot.replaceData(fullData);
+
+}
+function FinishGrid() {
+
+    if (objectsForGrid.length + stringsForGrid.length == 0)
+        return;
+
+    if (objectsForGrid.length > 0) {
+        AddObjectToFinishGrid();
+    }
+    else {
+        AddStringToFinishGrid();
+
+    }
+
     hot.clearFilter(true);
     hot.redraw(true);           
 }
