ebcc44098a4fd75974512f2d60a8c6759b8a9f13
Andrei Ignat <ignatandrei@yahoo.com>
2021-11-28 15:48:16 +0200

    added post with bearer

--- src/NetCore2Blockly/TestBlocklyHtml/Controllers/RestrictedAccessController.cs
+++ src/NetCore2Blockly/TestBlocklyHtml/Controllers/RestrictedAccessController.cs
@@ -21,5 +21,19 @@ namespace TestBlocklyHtml.Controllers
         {
             return Ok("AUth0:secret message that requires JWT authorization from Auth0");
         }
+
+        [HttpPost]
+        [Authorize(Policy = "Auth0Policy")]
+        public IActionResult Auth0SecretPost()
+        {
+            return Ok("POST AUth0:secret message that requires JWT authorization from Auth0");
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "CustomBearer")]
+        public IActionResult CustomJWTPost()
+        {
+            return Ok("POST: you can see a secret message that requires JWT authorization");
+        }
     }
 }
