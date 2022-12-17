725db59dfb225683729b35d9307e187ee06fe640
Andrei Ignat <ignatandrei@yahoo.com>
2022-12-17 10:13:03 +0200

    make azure app

---
+++ src/NetCore2Blockly/DeploySampleSiteAzure/Program.cs
@@ -0,0 +1,29 @@
+using NetCore2BlocklyNew;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseBlocklyUI(app.Environment);
+//after app.MapControllers();
+app.UseBlocklyAutomation();
+
+
+app.Run();
