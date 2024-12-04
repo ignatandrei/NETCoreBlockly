49d06b5c1b10a224e2581a983e5bc8ae2cba484a
Andrei Ignat <ignatandrei@yahoo.com>
2024-12-04 20:39:03 +0200

    adding more

---
+++ src/NetCore2Blockly/TestIntegration9/wwwroot/BlocklyAutomation/assets/showUsage/findBlocks.ps1
@@ -0,0 +1,21 @@
+cls
+cd demoBlocks
+$content = Get-Content -Path All.txt | ConvertFrom-Json 
+
+foreach($val in $content  ) {
+
+$file = $val.Id + ".txt" 
+
+Write-Host 'process' $file 
+$fileContent = [XML](Get-Content -Path $file)
+$blocks = $fileContent.SelectNodes("//*[local-name () = 'block']") | Select -Unique  -ExpandProperty type  | Where { $_.StartsWith("variables") -eq $false }
+$str =[String]::Join(';',$blocks)
+
+
+$val | add-member -Name "blocks" -value $str -MemberType NoteProperty -Force
+
+
+
+}
+
+$content | ConvertTo-Json | Out-File "All.txt" -Encoding utf8
